dd04b4a9276711d1c507ad10f8adc15198cfa26d
AfrozSheikh <sa8329925@gmail.com>
2024-06-16 10:19:08 +0530

    login page updated

---
+++ src/components/core/Dashboard/InstructorDashboard/InstructorChart.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+import {Chart, registerables} from "chart.js"
+
+Chart.register(...registerables);
+
+const InstructorChart = ({courses}) => {
+
+    // const [currChart, setCurrChart] = useState("students");
+
+    // //functio to genertae random colors
+    // const getRandomColors = (numColors) => {
+    //     const colors = [];
+    //     for(let i=0; i<numColors; i++) {
+    //         const color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random()*256)},
+    //         ${Math.floor(Math.random()*256)})`
+    //         colors.push(color);
+    //     }
+    //     return colors;
+    // }
+
+    // //create data for chart displaying student info
+
+    // const chartDataForStudents = {
+    //     labels: courses.map((course)=> course.courseName),
+    //     datasets: [
+    //         {
+    //             data: courses.map((course)=> course.totalStudentsEnrolled),
+    //             backgroundColor: getRandomColors(courses.length),
+    //         }
+    //     ]
+    // }
+
+
+    // //create data for chart displaying iincome info
+    // const chartDataForIncome = {
+    //     labels:courses.map((course)=> course.courseName),
+    //     datasets: [
+    //         {
+    //             data: courses.map((course)=> course.totalAmountGenerated),
+    //             backgroundColor: getRandomColors(courses.length),
+    //         }
+    //     ]
+    // }
+
+
+    //create options
+  
+
+  return (
+    // <div className=' bg-richblack-500 p-5 w-full h-250'>
+    //   <p className='font-bold'>Visualise</p>
+    //   <div className='flex gap-x-5'>
+    //     <button className='hover:text-yellow-25'
+    //     onClick={() => setCurrChart("students")}
+    //     >
+    //         Student
+    //     </button>
+
+    //     <button className='hover:text-yellow-25'
+    //     onClick={() => setCurrChart("income")}
+    //     >
+    //         Income
+    //     </button>
+    //   </div>
+    //   <div className='flex justify-center w-[300px] items-center m-auto'>
+    //     <Pie 
+    //         data={currChart === "students" ? chartDataForStudents : chartDataForIncome}
+    //         options={options}
+    //     />
+    //   </div>
+    // </div>
+    <div>Courses</div>
+  )
+}
+
+export default InstructorChart
