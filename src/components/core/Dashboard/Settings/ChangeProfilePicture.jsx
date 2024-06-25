95d6980d0439d77a012c31eb3cb3841c149ee659
Afroz Sheikh <147607954+AfrozSheikh@users.noreply.github.com>
2024-06-25 14:06:59 +0530

    Update ChangeProfilePicture.jsx

--- src/components/core/Dashboard/Settings/ChangeProfilePicture.jsx
+++ src/components/core/Dashboard/Settings/ChangeProfilePicture.jsx
@@ -60,7 +60,7 @@ export default function ChangeProfilePicture() {
   return (
     <>
       <div className="flex items-center justify-between rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-8 px-12 text-richblack-5">
-        <div className="flex items-center gap-x-4">
+         <div className="flex items-center flex-col md:flex-row gap-x-4">
           <img
             src={previewSource || user?.image}
             alt={`profile-${user?.firstName}`}
